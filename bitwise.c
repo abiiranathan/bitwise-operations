bc050100b10c9945679f397e0e0e76c5fc55c81a
Dr. Abiira Nathan <nabiira2by2@gmail.com>
2023-12-22 22:23:20 +0300

    Add more examples

--- bitwise.c
+++ bitwise.c
@@ -8,6 +8,8 @@
  Bitwise XOR(^)
  */
 #include <stdint.h>
+#include <stdio.h>
+void efficientMemoryAlloc();
 
 unsigned char A, B, C;  // 8-bit number(1 byte)
 
@@ -79,4 +81,117 @@ int main() {
 
   // Extract lower 8 bits
   REGT_8b = (REGHL_16 & 0xFF00);  // 10101011
+
+  // More advanced and real world examples
+  // Bit Manipulation for GPIO Handling:
+  // In embedded systems, GPIO (General Purpose Input/Output) pins
+  // are often controlled using bitwise operations.
+  // You can set, clear, toggle, or read individual bits to control
+  // various peripherals or interact with external devices.
+
+// Assuming GPIO_PORT is the register representing the GPIO port
+#define GPIO_PIN_2 (1 << 2)
+
+  volatile uint32_t GPIO_PORT = 0x40020000;
+
+  // Set Pin 2 to HIGH
+  GPIO_PORT |= GPIO_PIN_2;
+
+  // Clear Pin 2
+  GPIO_PORT &= ~GPIO_PIN_2;
+
+  // Toggle Pin 2
+  GPIO_PORT ^= GPIO_PIN_2;
+
+  // Read the state of Pin 2
+  if (GPIO_PORT & GPIO_PIN_2) {
+    // Pin 2 is HIGH
+  } else {
+    // Pin 2 is LOW
+  }
+
+
+  // Bitwise Operations for Device Configuration:
+  // Bitwise operations are frequently used for configuring device registers.
+  // Many embedded peripherals are controlled by setting or clearing specific
+  // bits in configuration registers.
+
+  // Assume a configuration register for a Timer/Counter
+  volatile uint32_t TIMER_CONFIG_REG = 0x40001000;
+
+  // Enable Timer with Prescaler 64
+  TIMER_CONFIG_REG |= (1 << 0) | (1 << 3) | (1 << 4);
+
+  // Disable Timer
+  TIMER_CONFIG_REG &= ~(1 << 0);
+
+  // Bitwise Operations for Efficient Data Storage
+  // In resource-constrained embedded systems, memory efficiency is crucial.
+  // Bitwise operations can be used for compact data storage and retrieval.
+
+  // Assume a data structure representing flags with limited memory
+  struct Flags {
+    unsigned int flag1 : 1;
+    unsigned int flag2 : 1;
+    unsigned int flag3 : 1;
+    unsigned int flag4 : 1;
+  };
+
+  struct Flags myFlags;
+  myFlags.flag1 = 1;
+  myFlags.flag2 = 0;
+  myFlags.flag3 = 0;
+  myFlags.flag4 = 1;
+
+  //   Bitwise Operations for Protocol Implementation:
+  // When working with communication protocols like I2C or SPI,
+  // bitwise operations are often used to encode and decode protocol-specific data.
+
+  // Assume a byte representing data to be sent over SPI
+  uint8_t spiData = 0b11011010;
+
+  // Extract specific bits for configuration
+  uint8_t configBits = (spiData >> 3) & 0x0F;
+
+  // Modify specific bits for transmission
+  spiData |= (1 << 1);  // Set a flag in the data byte
+
+  efficientMemoryAlloc();
+}
+
+void efficientMemoryAlloc() {
+// Define flags using bitwise operations
+#define FLAG_READ  (1 << 0)
+#define FLAG_WRITE (1 << 1)
+#define FLAG_EXEC  (1 << 2)
+
+  // Assume a memory pool represented by an array of bytes
+  uint8_t memoryPool[256];
+
+
+  // Allocate 4 bytes in the memory pool
+  uint8_t* allocatedMemory = &memoryPool[0];
+
+  // Set flags using bitwise OR
+  uint8_t permissions = FLAG_READ | FLAG_WRITE;
+
+  // Apply permissions to the allocated memory
+  *allocatedMemory |= permissions;
+
+  // Check if READ permission is set
+  if (*allocatedMemory & FLAG_READ) {
+    printf("Read permission is granted.\n");
+  }
+
+  // Check if WRITE permission is set
+  if (*allocatedMemory & FLAG_WRITE) {
+    printf("Write permission is granted.\n");
+  }
+
+  // Check if EXECUTE permission is set
+  if (*allocatedMemory & FLAG_EXEC) {
+    printf("Execute permission is granted.\n");
+  } else {
+    printf("Execute permission is not granted.\n");
+  }
 }
